4d6ea41e06689fb699b822e9f16f61759e4d6a92
nwroot <nwroot@protonmail.com>
2022-10-09 19:06:20 -0300

    added OrdenCompra, DetalleOrden, Articulo

---
+++ src/main/java/xyz/nwroot/udec/java/project1/DetalleOrden.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package xyz.nwroot.udec.java.project1;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author nwroot
+ */
+class DetalleOrden {
+            private Date fecha;
+    private String estado;
+    
+    private ArrayList<Articulo> articulos;
+    public int calcPrecioSinIVA() {
+        
+    }
+    
+    public int calcIVA() {
+        
+    }
+    
+    public int calcPrecio() {
+        
+    }
+    
+    public int calcPeso() {
+        
+    }
+}
---
+++ src/main/java/xyz/nwroot/udec/java/project1/OrdenCompra.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package xyz.nwroot.udec.java.project1;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class OrdenCompra {
+    private Date fecha;
+    private String estado;
+    
+    private ArrayList<DetalleOrden> ordenes;
+    
+    public int calcPrecioSinIVA() {
+        
+    }
+    
+    public int calcIVA() {
+        
+    }
+    
+    public int calcPrecio() {
+        
+    }
+    
+    public int calcPeso() {
+        
+    }
+}
